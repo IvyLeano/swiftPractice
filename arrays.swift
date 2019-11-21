105340df023414fb45ebddbcf90988b42cc5d867
unknown <IvyLeano@outlook.com>
2019-11-21 17:50:27 -0500

    updated arrays.swift

--- arrays.swift
+++ arrays.swift
@@ -1,15 +1,21 @@
 //array examples and syntax
+//empty arrays
 var uninitializedArray: [Double]
+var emptyArray1: [Double] = []
+var emptyDictionary: [String: String] = [:]  //to declare an empty key-value pair
+var array: Array<Float> = Array()
+var emptyArray2 = [String]()
+var emptyDictionary2 = [String: Float]()
+
+//initialized arrays
 var shoppingList = ["catfish", "water", "tulips"]
 var numbers = [1, 2, 3, 4, 5, 6]
 var occupations = [ // key-value pairs
     "Malcom":"Captain",
     "Kaylee":"Mechanic"
 ]
-var emptyDictionary: [String: String] = [:]  //to declare an empty key-value pair
 var emptyDoubles: [Double] = [ 1.7, 1.2, 1.7 ]
 var counts = Array(repeating: 1, count: 5)
-var array: Array<Float> = Array()
 
 //Remove, Add, Update and Insert elements
 shoppingList.append("roses")
@@ -46,6 +52,7 @@ print(array.count)
 // 3. can't use a variable before initializing it
 //    ex) print(uninitializedArray)
 // 4. can't access an index that is out of bounds
+// 5. arrays automatically grow as you add elements
 // from: https://developer.apple.com/documentation/swift/array
 
 //Run
