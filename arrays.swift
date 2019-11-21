eccefba3b9ffa21dfa033a029555dd01b6aeb2df
unknown <IvyLeano@outlook.com>
2019-11-21 14:41:39 -0500

    arrays.swift updated

---
+++ arrays.swift
@@ -0,0 +1,16 @@
+//array examples and syntax
+var shoppingList = ["catfish", "water", "tulips"]
+var occupations = [ // key:value pairs
+    "Malcom":"Captain",
+    "Kaylee":"Mechanic"
+]
+
+//Output
+print("shoppingList[0] is \(shoppingList[0]).")
+print("shoppingList is \(shoppingList)")
+print(occupations["Malcom"])
+
+
+//Notes:
+// 1. Commas are allowed after last element
+// 2. Trying to access occupations[0], for the key:value pair, would throw an error
