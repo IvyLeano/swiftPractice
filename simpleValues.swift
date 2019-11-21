87c3a3f6ff167995dfe9fefeb92dc1234915d6a2
unknown <IvyLeano@outlook.com>
2019-11-21 13:35:35 -0500

    commented simpleValues

---
+++ simpleValues.swift
@@ -0,0 +1,19 @@
+
+//variables
+
+//constants
+
+//implicit types - the compliler infer its type
+
+//explicit types
+//used when the initial value does not provide enough information
+//used when there is no initial value
+
+
+
+
+//Notes:
+// 1. values are never implicitly converted to another type - must do so explicity
+// ex) let label = "The width is"
+//    let width = 94
+//    let widthLabel = label + String(width)
