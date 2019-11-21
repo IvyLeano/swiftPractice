3306ac1880de3ce344ea226df6e87c99412e84e6
unknown <IvyLeano@outlook.com>
2019-11-21 16:54:01 -0500

    arrays.swift updated

--- arrays.swift
+++ arrays.swift
@@ -1,15 +1,28 @@
 //array examples and syntax
 var shoppingList = ["catfish", "water", "tulips"]
+var numbers = [1, 2, 3, 4, 5, 6]
 var occupations = [ // key:value pairs
     "Malcom":"Captain",
     "Kaylee":"Mechanic"
 ]
+var emptyDoubles: [Double] = [ 1.7, 1.2, 1.7 ]
+var counts = Array(repeating: 1, count: 5)
+var array: Array<Float> = Array()
 
+//Adding and removing elements
+
+//Accessing arrays
 //Output
 print("shoppingList[0] is \(shoppingList[0]).")
 print("shoppingList is \(shoppingList)")
-print(occupations["Malcom"])
+print(numbers)
+    //print(occupations["Malcom"])
+print(emptyDoubles)
+print(counts)
 
+//Formatted printing of Arrays
+
+//Array properties
 
 //Notes:
 // 1. Commas are allowed after last element
