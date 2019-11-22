032a15b5cd6d84a348787c070959b5289f16d2f2
unknown <IvyLeano@outlook.com>
2019-11-22 15:50:10 -0500

    tuple added

--- functionsAndClosures.swift
+++ functionsAndClosures.swift
@@ -16,3 +16,14 @@ func subtract(_ x: Int, _ y: Int) -> Int {
     return x + y
 }
 print("3 + 5 = \(subtract(3, 5))")
+
+//tuple: used to make a compound value - to return multiple values from a function
+func calculate(x: Int, y: Int) -> (Int, Int, Int) {
+    let add = x + y
+    let subtract = x - y
+    let divide = x / y
+
+    return (add, subtract, divide)
+}
+let calculate = calculate(x: 4, y: 2)
+print(calculate)
