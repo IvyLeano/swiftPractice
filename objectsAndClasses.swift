4f6d94257018a5d1e8fe64e55f97a44123d1824f
unknown <IvyLeano@outlook.com>
2019-11-25 15:15:46 -0500

    TriangleAndSquare class added

--- objectsAndClasses.swift
+++ objectsAndClasses.swift
@@ -80,6 +80,33 @@ print("Perimeter is: \(triangle.perimeter)")
 triangle.perimeter = 10
 print("Perimeter after set is called: \(triangle.perimeter)")
 
+// willset and didset - if you don't need to compute the property but still need to provide code
+// that is run before and after setting a new value.
+// the code provided is run anytime the value changes outside of an initializer
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+var triangleAndSquare = TriangleAndSquare(size:10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
 // Notes
 // 1. Every property needs a value assigned - either in its declaration or in its initializer
 // 2. There is no requirement for classes to sublcass any standard root class, include superclass as needed
