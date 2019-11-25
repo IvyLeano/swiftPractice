f699317a5fcc27b3b7d4a9c58980cb0006513036
unknown <IvyLeano@outlook.com>
2019-11-25 16:47:21 -0500

    enum Rank added

---
+++ enumerationsAndStructures.swift
@@ -0,0 +1,33 @@
+// 'enum': used to create an enumeration
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+    func simpleDescription() -> String {
+        switch self {
+            case .ace: 
+                return "ace"
+            case .jack:
+                return "jack"
+            case .queen:
+                return "queen"
+            case .king:
+                return "king"
+            default:
+                return String(self.rawValue)
+        }
+    }
+    let ace = Rank.ace
+    let aceRawValue = ace.rawValue
+}
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
+
+// Notes:
+// 1. enumerations can have methods associated with them
