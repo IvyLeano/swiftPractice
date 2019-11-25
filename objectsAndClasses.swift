59003be76b98b866744b440aa002e9e5f763729b
unknown <IvyLeano@outlook.com>
2019-11-25 12:14:51 -0500

    Shape initializer updataed

--- objectsAndClasses.swift
+++ objectsAndClasses.swift
@@ -10,17 +10,16 @@ class Shape {
    }
 
    func simpleDescription() -> String {
-       return "A shape with \(numberOfSides) sides."
+       return "A shape with \(numberOfSides) sides is a \(self.name)."
    } 
 }
 
 // Creating an instance of a class
-var shape = Shape()
-shape.numberOfSides = 7
+var shape = Shape(name: "square")
+shape.numberOfSides = 4
 var shapeDescription = shape.simpleDescription()
 
 print(shapeDescription)
-
 
 // Notes
 // 1. Every property needs a value assigned - either in its declaration or in its initializer
