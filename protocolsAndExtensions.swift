0ecfae59d03f77f184fe73b3e47a5de0d4c5619a
unknown <IvyLeano@outlook.com>
2019-11-27 15:11:06 -0500

    class with protocol example added

--- protocolsAndExtensions.swift
+++ protocolsAndExtensions.swift
@@ -1,6 +1,14 @@
 // 'protocol' keyword is used to declare a protocol
-
 protocol ExampleProtocol {
     var simpleDescription: String { get }
     mutating func adjust()
 }
+
+// classes, emumerations and structs can all use protocols
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+ }
