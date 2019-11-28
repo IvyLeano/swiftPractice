ac20743bcdd5c326e3050a41cb39194ca3bb2bc1
unknown <IvyLeano@outlook.com>
2019-11-28 12:12:38 -0500

    throw function added

--- errorHandling.swift
+++ errorHandling.swift
@@ -4,3 +4,12 @@ enum PrintError: Error {
     case noToner
     case onFire
 }
+
+// use 'throw' to throw an error
+// use 'throws' to mark a function that can throw an error
+func send(job: Int, toPrinterName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrintError.noToner
+    }
+    return "Job sent"
+}
