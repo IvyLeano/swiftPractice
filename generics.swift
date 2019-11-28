ea1f6c73a3ed665b7303b1024662073e53e9e379
unknown <IvyLeano@outlook.com>
2019-11-28 13:42:46 -0500

    where example

--- generics.swift
+++ generics.swift
@@ -16,5 +16,22 @@ enum OptionalValue<Wrapped> {
 var possibleInteger: OptionalValue<Int> = .none
 possibleInteger = .some(100)
 
+// use 'where' right before the body to specify a list of requirements
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Element: Equatable, T.Element == U.Element
+    {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+anyCommonElements([1,2,3],[3])
+
+
 // Notes:
 // 1. you can make generic forms of functions and methods as well as classes, enums and structs
+// 2. writing <T: Equatable> is the same as writing <T> ... where T: Equatable
