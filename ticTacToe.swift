af2fb24112733f54a89c7e4b08becd1825a8a0b6
unknown <IvyLeano@outlook.com>
2019-12-02 18:28:06 -0500

    TicTacToe class added

--- ticTacToe.swift
+++ ticTacToe.swift
@@ -3,3 +3,22 @@
 // 2. objects and classes
 // 3. printing statements
 // 4. simple values
+// 5. control flow
+
+class TicTacToe {
+    
+    var grid = [
+        [ false, false, false ],
+        [ false, false, false ],
+        [ false, false, false ]
+    ]
+    func printGrid(){
+        for row in 0...<3 {
+            for column in 0...<3 {
+
+            }
+        }
+    }
+}
+
+var ticTacToe = TicTacToe()
