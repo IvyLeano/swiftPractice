7093e978fcd963638de299c4b66d47a6ab7326f3
unknown <IvyLeano@outlook.com>
2019-12-04 12:16:47 -0500

    var and let demo

---
+++ Swift5EssentialTraining(HarrisonFerrone)/variablesAndConstants.swift
@@ -0,0 +1,17 @@
+// the keyword 'var' is used to indicate that the variable can be modified
+var currentHealth = 90
+
+// attempting to change the var currentHealth
+currentHealth = 80
+
+// the keyword 'let' is used to indicate that the variable cannot be modified 
+// the value is a constant
+let maxHealth = 100
+
+// attempting to change constant maxHealth
+// Note: the below line will throw an error
+// error: cannot assign to value: 'maxHealth' is a 'let' constant
+
+//maxHealth = 80
+
+
