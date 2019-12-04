3600931a36849c35887038a01db0e5addb82bf5e
unknown <IvyLeano@outlook.com>
2019-12-04 13:00:54 -0500

    loggind and commenting completed

---
+++ Swift5EssentialTraining(HarrisonFerrone)/loggingAndCommenting.swift
@@ -0,0 +1,17 @@
+// COMMENTING IN SWIFT
+
+// 1. single line comments
+
+/* 2. multi-line
+   comments */
+
+/* 3. nested comments
+      // this is a nested comment  
+*/
+
+// LOGGING IN SWIFT
+var helloWorld = "hello world!"
+print(helloWorld)
+
+
+
