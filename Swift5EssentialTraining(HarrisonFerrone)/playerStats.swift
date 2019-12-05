a405a44632b2c6e67626b81e6c3c715dd35323d5
unknown <IvyLeano@outlook.com>
2019-12-05 16:03:50 -0500

    playerStats completed

---
+++ Swift5EssentialTraining(HarrisonFerrone)/playerStats.swift
@@ -0,0 +1,21 @@
+
+// Tasks:
+// 1. 
+var characterName = "Tony Stark"
+var weaponClassification = "Repulsors"
+
+// 2. 
+characterName += " - Iron Man"
+
+// 3. 
+var currentMana = 9.45
+
+// 4.
+var manaPercentage = String(currentMana / 100)
+
+// 5.
+var debugStats = "Character: \(characterName)\nWeapon: \(weaponClassification)\nCurrentMana: \(currentMana)\nManaPercentage: \(manaPercentage)"
+
+// 6.
+var questAccepted = true
+var canQuest = true
