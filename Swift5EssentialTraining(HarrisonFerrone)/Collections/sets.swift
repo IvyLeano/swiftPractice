c55eed3fb25c8d3538c33ddd17e5c9f2be806395
unknown <IvyLeano@outlook.com>
2019-12-10 16:54:13 -0500

    sets updated

---
+++ Swift5EssentialTraining(HarrisonFerrone)/Collections/sets.swift
@@ -0,0 +1,16 @@
+// creating sets
+var activeQuest: Set<String> = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny Needs Firewood"]
+
+// inserting and removing elements
+activeQuest.remove(at: Set<String>.Index)
+activeQuests.remove("The Undertaker")
+
+
+print(activeQuests)
+
+// more common methods
+activeQuests.contains("All-4-One")
+activeQuests.sorted()
+activeQuests.in
+
+print(activeQuests)
