6f8d649da8ff25f4fc990f214da2872b835adf1d
unknown <IvyLeano@outlook.com>
2019-12-10 17:02:55 -0500

    working with sets

--- Swift5EssentialTraining(HarrisonFerrone)/Collections/sets.swift
+++ Swift5EssentialTraining(HarrisonFerrone)/Collections/sets.swift
@@ -1,16 +1,24 @@
 // creating sets
+// useful for storing unique values in an unordered collection
+
+// must specify that it is a set, else the default is an array
+// var activeQuest: Set = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny Needs Firewood"]
 var activeQuest: Set<String> = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny Needs Firewood"]
 
 // inserting and removing elements
-activeQuest.remove(at: Set<String>.Index)
+// efficient - index and key value pair references not required
+activeQuests.insert("Only the strong")
 activeQuests.remove("The Undertaker")
-
 
 print(activeQuests)
 
 // more common methods
 activeQuests.contains("All-4-One")
-activeQuests.sorted()
-activeQuests.in
+
+// alphabetically, can also take predicates
+activeQuests.sorted() 
+
+// sets can only store values that are hashable (can provide a hash value of themselves)
+// all of swifts basic types are hashable by default
 
 print(activeQuests)
