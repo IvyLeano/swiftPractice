1cc077414b4d0c4aef30e7046fab3de996272859
unknown <IvyLeano@outlook.com>
2019-12-11 12:32:18 -0500

    core set methods complete

--- Swift5EssentialTraining(HarrisonFerrone)/Collections/sets.swift
+++ Swift5EssentialTraining(HarrisonFerrone)/Collections/sets.swift
@@ -22,3 +22,30 @@ activeQuests.contains("All-4-One")
 // all of swifts basic types are hashable by default
 
 print(activeQuests)
+
+// core set methods
+var completedQuests: Set = ["Big Boss", "All-4-One", "The Hereafter"]
+
+// set operations
+// intersection returns the common values between activeQuests and completedQuests
+// returns "Big Boss"
+var commonQuests = activeQuests.intersection(completedQuests)
+
+// opposite of intersection - returns all values that are not shared
+var differentQuests = activeQuests.symmetricDifference(completedQuests)
+
+// creates a new set of all values in both sets
+var allQuests = activeQuests.union(completedQuests)
+
+// subtracting a set
+var clippedQuests = activeQuests.subtracting(completedQuests)
+
+
+// Note: 
+// to dynamically perform the above operations, directly on a set, use:
+// formUnion, formSymmetricDifference and subtractingMethods
+
+
+print(completedQuests)
+print(differentQuests)
+print(activeQuests)
