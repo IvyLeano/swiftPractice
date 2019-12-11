20485a453905b1c9567ba567ce39df4806992165
unknown <IvyLeano@outlook.com>
2019-12-11 12:34:00 -0500

    tuples added

---
+++ Swift5EssentialTraining(HarrisonFerrone)/Collections/tuples.swift
@@ -0,0 +1,2 @@
+// tuples: can take a group of values and store them as one compound value
+// similar to arrays, but tuples can store values of different types so that you can mix and match as needed
