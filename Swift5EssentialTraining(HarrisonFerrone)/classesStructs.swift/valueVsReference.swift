399da1a4d334a1208378ba0509c16789c113507a
unknown <IvyLeano@outlook.com>
2019-12-12 07:33:56 -0500

    value vs reference notes completed

---
+++ Swift5EssentialTraining(HarrisonFerrone)/classesStructs.swift/valueVsReference.swift
@@ -0,0 +1,14 @@
+// In swift there are 2 types:
+// 1. Value Types
+// 2. Referemce Types
+
+// All swift basic data types are implemented as structures (value types)
+// ex) strings, characters, arrays, dictionaries, booleans, integers, floating point numbers, enumerations
+
+// Functions, classes and closures are reference types.
+
+// Passing by value: the original value is copied and changes to the copy have no affect on the original.
+// Passing by reference: working with the original value
+
+// structs -> simple, encapsulated object needs + copied
+// classes -> anything needing to be passed by reference + uses inheritance
