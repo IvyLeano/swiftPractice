1c5e8c6edae2bcd0caf9680f59bda1197301180d
unknown <IvyLeano@outlook.com>
2019-12-12 13:30:18 -0500

    extensions completed

---
+++ Swift5EssentialTraining(HarrisonFerrone)/extensions.swift
@@ -0,0 +1,56 @@
+// declaring a protocol
+protocol Collectable {
+    var name: String { get }
+    var price: Int { get set }
+
+    init(withName: String, startingPrice: Int)
+    func collect() -> Bool
+}
+
+extension Collectable {
+    var priceIncrease: Int {
+        return self.price * 10
+    }
+    init(name: String){
+        self.init(withName: name, startingPrice: 100)
+    }
+    // func collect() -> Bool {
+    //     print("Default item couldn't be collected...")
+    //     return false
+    // }
+    
+}
+protocol Usable {
+    func use()
+}
+
+// protocol adoption
+class Item: Collectable, Usable {
+    var name: String
+    var price: Int
+    required init(withName: String, startingPrice: Int) {
+        self.name = withName
+        self.price = startingPrice
+    }
+    func collect() -> Bool {
+        print("Item collected!")
+        return true
+    }
+    func use() {
+        print("Item Used!")
+    }
+}
+let potion = Item(withName: "High Potion", startingPrice: 35)
+potion.collect()
+potion.use()
+
+let antidote = Item(name: "Antidote")
+print(antidote.price)
+
+extension String {
+    func fancyDebug() {
+        print("This string has \(self.count) characters")
+    }
+}
+
+antidote.name.fancyDebug()
