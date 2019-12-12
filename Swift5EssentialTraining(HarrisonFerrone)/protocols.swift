bb7d2fbf5f3b94b1658ca9df05987a75960a08c7
unknown <IvyLeano@outlook.com>
2019-12-12 13:23:19 -0500

    protocols added

---
+++ Swift5EssentialTraining(HarrisonFerrone)/protocols.swift
@@ -0,0 +1,34 @@
+// protocols can't store values but must have getter and setter methods
+
+// declaring a protocol
+protocol Collectable {
+    var name: String { get }
+    var price: Int { get set }
+
+    init(withName: String, startingPrice: Int)
+    func collect() -> Bool
+}
+
+ protocol Usable {
+    func use()
+}
+
+// protocol adoption
+class Item: Collectable, Usable {
+    var name: String
+    var price: Int
+    required init(withName: String, startingPrice: Int) {
+        self.name = withName
+        self.price = startingPrice
+    }
+    func collect() -> Bool {
+        print("Item collected!")
+        return true
+    }
+    func use() {
+        print("Item Used!")
+    }
+}
+let potion = Item(withName: "High Potion", startingPrice: 35)
+potion.collect()
+potion.use()
