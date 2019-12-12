8e9faa28c5d777911aa5e053b5a89a51331f01e3
unknown <IvyLeano@outlook.com>
2019-12-12 12:50:37 -0500

    enumerations completed

--- Swift5EssentialTraining(HarrisonFerrone)/Collections/enumerations.swift
+++ Swift5EssentialTraining(HarrisonFerrone)/Collections/enumerations.swift
@@ -1,3 +1,5 @@
+// REVIEW THIS TOPIC
+
 // enums: allow you to put related values into a custom type.
 // each value is a different case.
 
@@ -29,3 +31,33 @@ switch currentState {
 }
 
 // RAW AND ASSOCIATED VALUES
+
+// raw values
+enum NonPlayableCharacters: String {
+    case Villager = "Common, not much useful info there"
+    case BlackSmith = "One per village, will have quest information"
+    case Merchant = "No limit per village, will make you cool stuff"
+}
+var BlackSmith = NonPlayableCharacters.BlackSmith
+print(BlackSmith.rawValue)
+
+// associated values
+enum PlayerState {
+    case Alive
+    case KO(level: Int)
+    case Unknown(debugError: String)
+
+    func evaluateCase() {
+        switch self {
+            case .Alive:
+                print("Still Kicking")
+            case .KO(let restartLevel):
+                print("Sorry, back to level \(restartLevel) for you...")
+            case .Unknown(let message):
+                print(message)
+            default:
+                print("Unknown state encountered...")
+        }
+    }
+}
+PlayerState.KO(level: 1).evaluateCase()
