9373a89f3e7979dc4d55e9d6c45c295313ae522a
unknown <IvyLeano@outlook.com>
2019-12-17 13:28:43 -0500

    1. notes added

---
+++ StandfordUniversityCourse/One.IntroToIos.swift
@@ -0,0 +1,20 @@
+// Notes:
+// 1. What's in ios?
+//     Layers:
+//     a. Core OS: unix OS, C, non-oop, closest to hardware
+//     b. Core services: foundation, OOP layer, networking, file system, non UI layer
+//     c. Media: 3D graphics, video, audio, PDF, JPEG ...
+//     d. Cocoa touch layer: multi-touch, controls, camera, alerts... UI 
+// 2. calculator demo
+// 3. Topics
+//     a. creating projects in Xcode 8
+//     b. building a UI
+//     c. print
+//     d. actions and outlets
+//     e. accessing ios docs from code
+//     f. optionals
+//     g. ios simulator
+    
+
+    
+
